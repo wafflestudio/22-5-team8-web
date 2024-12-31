c18105ef55299cfa3e1811390f269bdf6a3196ce
knoj014 <jongkyung2005@gmail.com>
2024-12-31 18:58:25 +0900

    frontend setup
    frontend seminar에서 쓰던 template을 활용함
    tailwind CSS 쓸 수 있게 설정함

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+export const App = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-center text-red-500">
+        Watcha Pedia Clone Project
+      </h1>
+    </div>
+  );
+};
