b922ae4b7fb8639872e85632dacc9c3508442528
alexander050211 <70711686+alexander050211@users.noreply.github.com>
2025-01-31 05:17:11 +0900

    calendar design fix

--- src/components/MovieCalendar.tsx
+++ src/components/MovieCalendar.tsx
@@ -1,4 +1,5 @@
 import 'react-calendar/dist/Calendar.css';
+import 'moment/locale/ko';
 
 import moment from 'moment';
 import { useState } from 'react';
@@ -53,6 +54,19 @@ const MovieCalendar = () => {
   const tileContent = ({ date, view }: { date: Date; view: string }) => {
     // Show either the first movie poster or the day number
     if (view === 'month') {
+      const isSameMonth = date.getMonth() === activeStartDate.getMonth() && date.getFullYear() === activeStartDate.getFullYear();
+      if(!isSameMonth) {
+        return (
+          <div className='absolute inset-0 flex items-center justify-center bg-white'>
+            <img
+              src="https://placehold.co/120x160?text=AltMonth"
+              alt="Neighboring Month"
+              className="w-full h-full object-cover"
+            />
+          </div>
+        )
+      }
+
       const dateKey = moment(date).format('YYYY-MM-DD');
       const movies = moviesWatchedByDate[dateKey];
       if (movies !== undefined && movies.length > 0) {
@@ -68,7 +82,7 @@ const MovieCalendar = () => {
       } else {
         // For days without movies, show the numeric date
         return (
-          <div className='absolute inset-0 flex items-center justify-center'>
+          <div className='absolute inset-0 flex items-center justify-center font-medium text-gray-400 text-lg'>
             {moment(date).format('D')}
           </div>
         );
@@ -84,13 +98,14 @@ const MovieCalendar = () => {
         <button
           onClick={handleCurrentMonth}
           className={`p-2 bg-white text-gray-400 rounded-md border-2
-            ${!isCurrentMonth() ? 'border-black' : 'border-white'}`}
+            ${!isCurrentMonth() ? 'border-gray-400' : 'border-white'}`}
         >
           오늘
         </button>
       </div>
 
       <Calendar
+        locale='ko'
         className="react-calendar border-0 shadow-none"
         tileClassName={() => 'relative p-0 border-0 aspect-[2/3] overflow-hidden'}
         tileContent={tileContent}
@@ -105,12 +120,24 @@ const MovieCalendar = () => {
         onChange={(date) => {
           setSelectedDate(date as Date);
         }}
+        prevLabel={<span className="text-4xl font-bold text-gray-300 px-2">‹</span>}
+        nextLabel={<span className="text-4xl font-bold text-gray-300 px-2">›</span>}
+        navigationLabel={({ date }) => (
+          <span className="text-lg font-bold mx-0 px-0">
+            {moment(date).format('YYYY.M')}
+          </span>
+        )}
         formatDay={() => ''}
         formatMonthYear={(_, date) => moment(date).format('YYYY.M')}
+        formatShortWeekday={(_, date) => {
+          const formatted = moment(date).locale('ko').format('dd')[0];
+          if(formatted === undefined) return '';
+          return formatted;
+        }}
         calendarType="gregory"
         prev2Label={null}
         next2Label={null}
-        showNeighboringMonth={false}
+
         minDetail="month"
         maxDetail="month"
         onClickDay={handleDayClick}
